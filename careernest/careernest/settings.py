adc5c851a2aea473c43dcb1334ddc780e4830c4e
Ashmil K <ashmilk114@gmail.com>
2024-03-16 20:28:16 +0530

    Vercel Deployment setup

--- careernest/careernest/settings.py
+++ careernest/careernest/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=gkxgw4otb$-(w-qw3@@=i9)($mc#vh+me0ykpruf_si@_b1y0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -40,11 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'User',
     'Staff',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ALLOW_ALL_ORIGINS:True
 
 ROOT_URLCONF = 'careernest.urls'
 
