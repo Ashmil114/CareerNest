8345bd24d21ee3a3c18d3bc1e9518c2f3d6a0fb2
Ashmil K <ashmilk114@gmail.com>
2024-03-17 22:58:28 +0530

    Add CustomLogin

--- careernest/User/urls.py
+++ careernest/User/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import StudentsView,SignUpView,StudentDataView
-from rest_framework.authtoken.views import obtain_auth_token
+from .views import StudentsView,SignUpView,StudentDataView,CustomAuthToken
+# from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('',StudentsView.as_view()),
     path('signup',SignUpView.as_view()),
-    path('login',obtain_auth_token,name='login'),
+    path('login',CustomAuthToken.as_view(),name='login'),
     path('user-onboard/<int:pk>',StudentDataView.as_view()),
     
     
--- careernest/User/views.py
+++ careernest/User/views.py
@@ -2,6 +2,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 from rest_framework import status
 
 # Models
@@ -63,3 +65,17 @@ class SignUpView(APIView):
     
         
 
+class CustomAuthToken(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email,
+            'username':user.username
+        })
